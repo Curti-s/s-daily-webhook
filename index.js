293862d970351175ba9da4ecd6c7280496a9f058
k1r1m1 <kirimi@paygoenergy.co>
2020-10-20 13:21:04 +0300

    serve webhook on port 4567

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 require('dotenv').config();
-const axios = require('axios');
 
-(async () => {
-  try {
-    const { status } = await axios.post('https://api.github.com/repos/Curti-s/s-daily/actions/workflows/deploy.yml/dispatches',
-      { ref:'master' },
-      { headers: {
-          Accept:'application/vnd.github/v3+json',
-          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-        }
-      });
-    
-    if(status === 204) {
-      console.log('successfully triggered deploy hook w/ statusCode ', 200);
-      return { statusCode:200, body:'Deploy webhook called.' }
-    } else {
-      throw new Error(status);
+const axios = require('axios');
+const express = require('express');
+
+const app = express();
+const PORT = 4567;
+
+app.get('/', (req, res) => {
+  const handler = async () => {
+    try {
+      const { status } = await axios.post('https://api.github.com/repos/Curti-s/s-daily/actions/workflows/deploy.yml/dispatches',
+        { ref:'master' },
+        { headers: {
+            Accept:'application/vnd.github/v3+json',
+            Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+          }
+        });
+
+      if(status === 204) {
+        res.send(`successfully triggered deploy hook w/ statusCode:${status}`)
+      } else {
+        throw new Error(status);
+      }
+    } catch(err) {
+      res.send(`An error occurred ${err.mesage}`);
     }
-  } catch(err) {
-    console.log('An error occurred', err);
-  }
-})();
+  };
+  handler();
+});
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
